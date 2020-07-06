9bb8ba50f5ff985af5c3c4a6ef018e9fcc64eab1
Diana Birkelbach <diana.birkelbach@orbis.de>
2020-07-06 22:26:01 +0200

    it's colorful now

--- ColorfulOptionsetGrid/App/ColorfulGrid.tsx
+++ ColorfulOptionsetGrid/App/ColorfulGrid.tsx
@@ -14,9 +14,14 @@ export interface IColorfulGridProps{
 }
 
 export const ColorfulGrid = ({dataset, utils} : IColorfulGridProps) : JSX.Element => {
-    const optionSetColumns = ["column1", "column2", "column3", "column4"].map((alias) => dataset.columns.find((column) => column.alias===alias)?.name ?? alias); 
+    const customizedColors = dataset.columns.filter((column) => ["column1", "column2", "column3"].includes(column.alias));    
+    //found customized, or take all optionset columns otherwise
+    const optionSetColumns = (customizedColors.length >0 ? customizedColors : dataset.columns.filter((column) => column.dataType==="OptionSet")).map((column) => column.name);
+
+    //const optionSetColumns = ["column1", "column2", "column3", "column4"].map((alias) => dataset.columns.find((column) => column.alias===alias)?.name ?? alias); 
     const metadataAttributes = useGetAttributes(dataset.getTargetEntityType(), optionSetColumns, utils );
     const columns = dataset.columns.map((column) : IColumn => {
+        const meta = metadataAttributes?.options.get(column.name);
         return {
             key: column.name,
             name : column.displayName,             
@@ -24,16 +29,18 @@ export const ColorfulGrid = ({dataset, utils} : IColorfulGridProps) : JSX.Elemen
             minWidth : column.visualSizeFactor,
             maxWidth: 300,            
             isResizable: true, 
-            onRender: (item : any) => {
-                return  <div> <Icon className="colorIcon" style={{color: "red"}} iconName="CircleShapeSolid" aria-hidden="true" /><span>{item[column.name]}</span></div>  
-              },                  
+            onRender: column.dataType==="OptionSet"  ? (item : any) => {
+                return  <div> <Icon className="colorIcon" style={{color: meta?.get(item.raw.getValue(column.name)) ?? "white", marginRight: "5px"}} iconName="CircleShapeSolid" aria-hidden="true" /><span>{item[column.name]}</span></div>  
+              } : undefined,                  
         };
     });   
     const items = dataset.sortedRecordIds.map((id) => {                
         const entityIn = dataset.records[id];
         const attributes = dataset.columns.map((column) => ({[column.name]: entityIn.getFormattedValue(column.name)}));
         return Object.assign({
-            key: entityIn.getRecordId()},
+                key: entityIn.getRecordId(),
+                raw : entityIn
+            },
             ...attributes)
     });        
     return (<DetailsList items={items} columns={columns}></DetailsList>);
--- ColorfulOptionsetGrid/App/Hooks/useGetMetadata.ts
+++ ColorfulOptionsetGrid/App/Hooks/useGetMetadata.ts
@@ -2,25 +2,26 @@ import React = require("react");
 
 
 export const useGetAttributes = (entityName : string, attributeNames : string[], utils: ComponentFramework.Utility) => {
-    const [options, setOptions] = React.useState<Map<string, ComponentFramework.PropertyHelper.OptionMetadata[]>>(new Map());
+    const [options, setOptions] = React.useState<Map<string, Map<string, string>>>(new Map());
     
 
     React.useEffect(() => {
+
         utils.getEntityMetadata(entityName, attributeNames)
         .then((entityMetadata) => {
-            const opts = attributeNames.map((attributeName) => {
-                const thisOptions : ComponentFramework.PropertyHelper.OptionMetadata[]= entityMetadata.Attributes.get(attributeName)?.attributeDescriptor.OptionSet ?? [];
-                return [attributeName, thisOptions] as [string,  ComponentFramework.PropertyHelper.OptionMetadata[]];
+            const opts = attributeNames.map((attributeName) => {                
+                const thisOptions : []= (entityMetadata.Attributes.get(attributeName)?.attributeDescriptor.OptionSet ?? []).map((option : any) => [option.Value.toString(), option.Color] );
+                return [attributeName, new Map(thisOptions)] as [string,  Map<string, string>];
             } )
             console.log(opts);
             //todo implement fallback per webapi
             setOptions(new Map(opts));
         })
+        .catch(console.error);
 
     }, [entityName, ...attributeNames]);
 
-    return 
-    {
+    return {
         options
     };
 
