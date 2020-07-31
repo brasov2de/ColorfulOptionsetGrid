86417046ce9a3bc436a9a74068a6d692b6f0dfcd
Diana Birkelbach <diana.birkelbach@orbis.de>
2020-07-31 18:26:41 +0200

    small changes

--- ColorfulOptionsetGrid/Data/EnvVarSchema.ts
+++ ColorfulOptionsetGrid/Data/EnvVarSchema.ts
@@ -3,3 +3,5 @@ import { ISetupSchema } from "../App/Model/interfaces"
 
 export const Example_Env_Var_Ampel : ISetupSchema = {"434350002": {"icon": "Sad", "color": "darkred"}, "434350001": {"icon": "EmojiNeutral", "color": "orange"}, "434350000":{"icon":"Emoji2", "color": "darkgreen"}, "434350003":{ "color": "white"}}
 export const Example_Env_Var_Ampel_Short : ISetupSchema = {"434350002": {"icon": "Sad"}, "434350001": {"icon": "EmojiNeutral"}, "434350000":{"icon":"Emoji2"}, "434350003":{ "color": "white"}}
+
+//"{'434350002': {'icon':'Sad'}, '434350001': {'icon': 'EmojiNeutral'}, '434350000':{'icon':'Emoji2'}, '434350003':{ 'color': 'gray'}}"
