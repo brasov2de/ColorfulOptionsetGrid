84f359f38dceabec6631bd32bebb6db7f349c5b4
Diana Birkelbach <diana.birkelbach@orbis.de>
2020-07-31 20:47:27 +0200

    screenshots

--- ColorfulOptionsetGrid/App/Cells/ColorfulCell.tsx
+++ ColorfulOptionsetGrid/App/Cells/ColorfulCell.tsx
@@ -8,15 +8,15 @@ export interface IColorfulCellProps {
     column: IGridColumn;
     metadataOptions :   Map<string, ISetupSchemaValue> | undefined;
     displayTextType: "SIMPLE" | "BOX" | "BORDER";    
-    displayIconType : "NONE" | "NAME" | "CONFIG";//| "ENVIRONMENT";
+    displayIconType : "NONE" | "NAME";//| "ENVIRONMENT";
     defaultIcon: string;       
 }
 
 export const ColorfulCell = function ColorfulCell({item, column, metadataOptions, displayTextType, displayIconType, defaultIcon} : IColorfulCellProps) : JSX.Element{    
     const currentOptionSetValue=  item.raw.getValue(column.original.name) as number;    
-    let color = metadataOptions?.get(currentOptionSetValue?.toString() ?? "")?.color ?? "black";  
+    let color = metadataOptions?.get(currentOptionSetValue?.toString() ?? "")?.color ?? "gray";  
     if(color==="white"){
-        color = "black;"
+        color = "gray"
     }
     const icon  = metadataOptions?.get(currentOptionSetValue?.toString() ?? "")?.icon ?? defaultIcon;  
     const iconColor = displayTextType==="BOX" ? "white" : color;
@@ -30,7 +30,7 @@ export const ColorfulCell = function ColorfulCell({item, column, metadataOptions
             borderRadius: "5px"
         },
         "BOX" : {
-            backgroundColor: color==="white" ? "black" : color, color: iconColor, borderRadius: "5px"
+            backgroundColor: color==="white" ? "gray" : color, color: iconColor, borderRadius: "5px"
         }, 
         "SIMPLE" : {             
         }
--- ColorfulOptionsetGrid/Data/EnvVarSchema.ts
+++ ColorfulOptionsetGrid/Data/EnvVarSchema.ts
@@ -2,6 +2,6 @@ import { ISetupSchema } from "../App/Model/interfaces"
 
 
 export const Example_Env_Var_Ampel : ISetupSchema = {"434350002": {"icon": "Sad", "color": "darkred"}, "434350001": {"icon": "EmojiNeutral", "color": "orange"}, "434350000":{"icon":"Emoji2", "color": "darkgreen"}, "434350003":{ "color": "white"}}
-export const Example_Env_Var_Ampel_Short : ISetupSchema = {"434350002": {"icon": "Sad"}, "434350001": {"icon": "EmojiNeutral"}, "434350000":{"icon":"Emoji2"}, "434350003":{ "color": "white"}}
+export const Example_Env_Var_Ampel_Short : ISetupSchema = {"434350002": {"icon": "Sad"}, "434350001": {"icon": "EmojiNeutral"}, "434350000":{"icon":"Emoji2"}, "434350003":{ "color": "black"}}
 
 //"{'434350002': {'icon':'Sad'}, '434350001': {'icon': 'EmojiNeutral'}, '434350000':{'icon':'Emoji2'}, '434350003':{ 'color': 'gray'}}"
